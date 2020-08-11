b7b07e3a27967a845907a55fca8568002caa2820
Seth Maxwell <sethmaxwl@gmail.com>
2020-08-11 19:13:52 +0000

    Remove unused config

--- setup.py
+++
@@ -1,8 +0,0 @@
-import os
-import random
-import string
-
-def generateKey():
-    return "".join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
-
-os.environ["SECRET_KEY"] = generateKey()
